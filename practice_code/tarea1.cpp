83572ac93a79a9fdd0fa67a187121e5cb4597bee
Luis Alberto Ballado Aradias <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2023-01-12 12:37:31 -0600

    anotaciones en codigo

--- practice_code/tarea1.cpp
+++ practice_code/tarea1.cpp
@@ -3,32 +3,41 @@
 
 //Funcion dummy para revisar si un
 //elemento esta dentro de un vector
+//Al tener un for con entrada n,
+//la funcion tiene complejidad O(n)
 bool existe(std::vector<int> _vector_, int valor){
-  for(int i = 0; i < _vector_.size(); i++){
-    if (_vector_[i] == valor){
+  for(int i = 0; i < _vector_.size(); i++){ // n
+    if (_vector_[i] == valor){              // 1
       return true;
     }
   }
   return false;
 }
 
+//Complejidad funcion principal O(n²)
+//por el doble for que recorre los arreglos
 int main(){
 
-  int a[] = {2,5,5,5}; //array 1
-  int b[] = {2,2,3,5,5,7}; //array 2
-  
-  std::vector<int>arr; //vector de resultados
+  int a[] = {2,5,5,5};                     // 1
+  int b[] = {2,2,3,5,5,7};                 // 1
 
-  int a_length = sizeof(a)/sizeof(a[0]); //longitud array1
-  int b_length = sizeof(b)/sizeof(b[0]); //longitud array2
+  //vector de resultados
+  std::vector<int>arr;                     // 1
+
+  //Solucion tomada del ejemplo 3
+  //https://www.digitalocean.com/community/tutorials/find-array-length-in-c-plus-plus
+  //longitud array1
+  int a_length = sizeof(a)/sizeof(a[0]);   // 1
+  //longitud array2
+  int b_length = sizeof(b)/sizeof(b[0]);   // 1
   
-  //Usando fuerza bruta
-  for (int i = 0; i < a_length; i++){
-    for (int j = 0; j < b_length; j++){
-      if (a[i] == b[j]){
+  //Usando fuerza bruta                    // O(n²)
+  for (int i = 0; i < a_length; i++){      
+    for (int j = 0; j < b_length; j++){    
+      if (a[i] == b[j]){                   // 1
 	//revisar si ya existe en el array donde se guarda
-	if (!existe(arr,a[i])){
-	  arr.push_back(a[i]);
+	if (!existe(arr,a[i])){            // 1
+	  arr.push_back(a[i]);             
 	}
 	break;
       }
@@ -39,9 +48,9 @@ int main(){
   // Imprimir resultado
   //--------------------
   
-  std::cout << "El resultado es: \n";
-  for(int i = 0; i<arr.size(); i++){
-    std::cout << arr[i] << "\n";
+  std::cout << "El resultado es: \n";       // 1
+  for(int i = 0; i<arr.size(); i++){        // n
+    std::cout << arr[i] << "\n";            // 1
   }
   
 }
