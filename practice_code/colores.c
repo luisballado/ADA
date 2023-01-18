2236ad3fbb369e3b57745915866ddadcbb696fe3
Luis Alberto Ballado Aradias <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2023-01-17 20:49:20 -0600

    pruebas del wavefront

---
+++ practice_code/colores.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+  printf("\033[31mred text\n");
+  printf("\033[33;44myellow on blue\n");
+  printf("\033[0mdefault colors\n");
+  return 0;
+}
---
+++ practice_code/colores.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+int main(){
+
+  /**
+  for(int counter=1;counter<=5;counter++){
+    system("clear");
+    printf("\033[31mred text\n");
+    printf("\033[33;44myellow on blue\n");
+    printf("\033[0mdefault colors\n");
+    }**/
+
+  std::cout << std::endl; //salto de linea
+
+  int size_x = 10;
+  int size_y = 15;
+  char a[size_x][size_y];
+  int x = 0;
+
+  //robot position
+  int robot_x = 6;
+  int robot_y = 6;
+
+  //goal position
+  int goal_x = 8;
+  int goal_y = 13;
+  
+  //cargar la matriz
+  for(int i = 0; i < size_x; i++){
+    for(int j = 0; j < size_y; j++){
+      a[i][j] = 'o';
+    }
+  }
+
+  //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
+  for(int i = 0; i < size_x; i++){
+    for(int j = 0; j < size_y; j++){
+      std::cout << a[i][j] << " ";
+    }
+    //salto de linea
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl; //salto de linea
+
+  //Superficies poner una X
+  for(int i = 0; i < size_x; ++i){
+    for(int j = 0; j < size_y; ++j){
+      if(i == 0 || i == size_x - 1 || j == 0 || j == size_y - 1){
+	a[i][j] = 'X';
+      }
+    }
+  }
+    
+  //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
+  
+  //xx
+  for(int i = 0; i < size_x; i++){
+    for(int j = 0; j < size_y; j++){
+      /**
+      if(a[i][j] == 'o'){
+	a[i][j] = 'o';
+      }
+      **/
+      //std::cout << '\a'; //hacer un sonido de beep
+      
+      if(i == robot_x and j == robot_y){
+	a[i][j] = 'R';
+      }else if(i == goal_x and j == goal_y) {
+	a[i][j] = 'G';
+      }
+
+      std::cout << a[i][j] << " ";
+    }
+    //salto de linea
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl; //salto de linea
+  
+  return 0;
+}
