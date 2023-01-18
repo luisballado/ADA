a15be99469fb40f7bed62bce98c56c916984ff17
Luis Alberto Ballado Aradias <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2023-01-18 11:56:07 -0600

    intento de animar

--- practice_code/colores.cpp
+++ practice_code/colores.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 #include <string>
 
+#include <chrono>
+#include <thread>
+
+#include <unistd.h>
+#include <cstdlib>
+
 int main(){
 
   /**
@@ -13,7 +19,7 @@ int main(){
     printf("\033[0mdefault colors\n");
     }**/
 
-  std::cout << std::endl; //salto de linea
+  //std::cout << std::endl; //salto de linea
 
   int size_x = 10;
   int size_y = 15;
@@ -27,59 +33,74 @@ int main(){
   //goal position
   int goal_x = 8;
   int goal_y = 13;
-  
-  //cargar la matriz
-  for(int i = 0; i < size_x; i++){
-    for(int j = 0; j < size_y; j++){
-      a[i][j] = 'o';
-    }
-  }
 
-  //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
-  for(int i = 0; i < size_x; i++){
-    for(int j = 0; j < size_y; j++){
-      std::cout << a[i][j] << " ";
-    }
-    //salto de linea
-    std::cout << std::endl;
-  }
-
-  std::cout << std::endl; //salto de linea
-
-  //Superficies poner una X
-  for(int i = 0; i < size_x; ++i){
-    for(int j = 0; j < size_y; ++j){
-      if(i == 0 || i == size_x - 1 || j == 0 || j == size_y - 1){
-	a[i][j] = 'X';
-      }
-    }
-  }
+  for(int i=0; i<10;i++){
     
-  //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
-  
-  //xx
-  for(int i = 0; i < size_x; i++){
-    for(int j = 0; j < size_y; j++){
-      /**
-      if(a[i][j] == 'o'){
+    system("clear");
+    //std::cout << std::endl; //salto de linea
+    
+    //cargar la matriz
+    for(int i = 0; i < size_x; i++){
+      for(int j = 0; j < size_y; j++){
 	a[i][j] = 'o';
       }
-      **/
-      //std::cout << '\a'; //hacer un sonido de beep
-      
-      if(i == robot_x and j == robot_y){
-	a[i][j] = 'R';
-      }else if(i == goal_x and j == goal_y) {
-	a[i][j] = 'G';
-      }
-
-      std::cout << a[i][j] << " ";
     }
-    //salto de linea
-    std::cout << std::endl;
-  }
 
-  std::cout << std::endl; //salto de linea
+    //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
+    for(int i = 0; i < size_x; i++){
+      for(int j = 0; j < size_y; j++){
+	std::cout << a[i][j] << " ";
+      }
+      //salto de linea
+      std::cout << std::endl;
+    }
+
+    sleep(1);
+    system("clear");
+    
+    //Superficies poner una X
+    for(int i = 0; i < size_x; ++i){
+      for(int j = 0; j < size_y; ++j){
+	if(i == 0 || i == size_x - 1 || j == 0 || j == size_y - 1){
+	  a[i][j] = 'X';
+	}
+      }
+    }
+    
+    //IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-IMPRIMIR-
+
+    //std::cout << std::endl; //salto de linea
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    
+    
+    
+    //xx
+    for(int i = 0; i < size_x; i++){
+      for(int j = 0; j < size_y; j++){
+	/**
+	   if(a[i][j] == 'o'){
+	   a[i][j] = 'o';
+	   }
+	**/
+	//std::cout << '\a'; //hacer un sonido de beep
+	
+	if(i == robot_x and j == robot_y){
+	  a[i][j] = 'R';
+	}else if(i == goal_x and j == goal_y) {
+	  a[i][j] = 'G';
+	}
+	
+	std::cout << a[i][j] << " ";
+      }
+      //salto de linea
+      std::cout << std::endl;
+    }
+    
+    std::cout << std::endl; //salto de linea
+    
+  }
+  
+  
   
   return 0;
 }
