781f20bdfc939f1e8c9ca601975d834ddc09528c
Luis Ballado <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2023-02-20 17:59:43 -0600

    modificaciones codigo

--- practice_code/tarea4_contenedores.cpp
+++ practice_code/tarea4_contenedores.cpp
@@ -2,21 +2,24 @@
 #include <vector>
 #include <stack>
 
-const int N = 3;
-const int TARGET = 2;
+//g++ -std=c++11 tarea4_contenedores.cpp -o contenedor
+
+//constantes nos permite declarar constantes de una manera más adecuada
+const int CONTENEDORES = 3;
+const int LITROS_OBJETIVOS = 2;
 
 // Define the initial state of the containers
 std::vector<int> initial_state = {0, 7, 4};
 
 // Define the goal state
 bool is_goal_state(const std::vector<int>& state) {
-  return state.at(1) == TARGET || state.at(2) == TARGET;
+  return state.at(1) == LITROS_OBJETIVOS || state.at(2) == LITROS_OBJETIVOS;
 }
 
 // Define the pouring operation
 std::vector<int> pour(const std::vector<int>& state, int from, int to) {
   std::vector<int> new_state(state);
-  int space = N - to;
+  int space = CONTENEDORES - to;
   int amount = std::min(space, state[from]);
   new_state.at(from) -= amount;
   new_state.at(to) += amount;
@@ -35,8 +38,8 @@ bool dfs() {
       return true;
     }
     visited.push_back(state);
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
+    for (int i = 0; i < CONTENEDORES; i++) {
+      for (int j = 0; j < CONTENEDORES; j++) {
 	if (i != j) {
 	  std::vector<int> new_state = pour(state, i, j);
 	  bool is_new = true;
@@ -59,9 +62,9 @@ bool dfs() {
 int main() {
   bool result = dfs();
   if (result) {
-    std::cout << "Si existe un camino donde quedan " << TARGET << " litros en los contenedores" << std::endl;
+    std::cout << "Si existe un camino donde quedan " << LITROS_OBJETIVOS << " litros en los contenedores" << std::endl;
   } else {
-    std::cout << "No existe un camino donde quedan " << TARGET << " litros en los contenedores" << std::endl;
+    std::cout << "No existe un camino donde quedan " << LITROS_OBJETIVOS << " litros en los contenedores" << std::endl;
   }
   return 0;
 }
