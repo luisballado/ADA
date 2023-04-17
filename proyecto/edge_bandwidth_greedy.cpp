85595fcf418ce20a4b3767fbb394af66bdf3f7fd
Luis Ballado <luis@madlab.mx>
2023-04-17 13:50:53 -0600

    solucion canonica y greedy

--- proyecto/edge_bandwidth_greedy.cpp
+++ proyecto/edge_bandwidth_greedy.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
       if(std::string(argv[i]) == " -solucion" && i + 1 < argc){
 	solucion = argv[i+1];
 
-	//TODO SWITH entre soluciones
+	
 	
       }
     }
@@ -79,7 +79,8 @@ int main(int argc, char* argv[]) {
 
     if(u<v){
       //no inicializar con pesos
-      map_lista_aristas.insert(std::make_pair(std::make_pair(u,v),i+1));
+      //map_lista_aristas <pair,bandwidth_id>
+      map_lista_aristas.insert(std::make_pair(std::make_pair(u,v),i));
       lista_aristas.push_back(std::make_pair(u,v)); //agregar 
     }
   }
@@ -98,19 +99,7 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "]" << std::endl;
   }
-  
-  //crear lista de aristas
-  //depreciado por ser n*n
-  /**
-  for(int i = 0; i < grafo.size(); i++){
-    for(int j = 0; j < grafo[i].size(); j++){
-      if(i<grafo[i][j]){
-	lista_aristas.push_back(std::make_pair(i, grafo[i][j])); //agregar
-      }
-    }
-  }
-  */
-    
+      
   // Ordenar vertices por grado en orden descendente (el de mayor grado primero)
   // https://www.digitalocean.com/community/tutorials/sort-in-c-plus-plus
   //size, nodo
@@ -124,14 +113,7 @@ int main(int argc, char* argv[]) {
   // https://www.geeksforgeeks.org/vector-rbegin-and-rend-function-in-c-stl/
   sort(grados.rbegin(), grados.rend());
 
-  std::cout << "############################" << std::endl;
-  std::cout << "NODO-GRADO" << std::endl;
-  //imprimir nodo-grado
-  for (int i = 0; i < grados.size(); i++){
-    std::cout << "NODO: " << grados[i].second
-	      << " GRADO:" << grados[i].first << std::endl;
-  };
-
+    
   std::cout << "############################" << std::endl;
   std::cout << "LISTA-ARISTA" << std::endl;
 
@@ -149,74 +131,177 @@ int main(int argc, char* argv[]) {
 	      << "PESO: " << bandwidth[i] << std::endl;
   };
 
+  /***
   std::cout << "############################" << std::endl;
   std::cout << "LISTA-ARISTA CON MAP" << std::endl;
   
   //lista de aristas map
   for (auto const& arista : map_lista_aristas) {
     std::cout << "arista: (" << arista.first.first << ", " << arista.first.second << ")";
-    std::cout << " peso: " << arista.second << std::endl;
-  }
-
-  //vector max bandwidth
-  std::vector<int> max_bandwidth;
-
-  
-  
-  //print bandwidths of each edge
-  /***
-  for(int u=1; u<n; u++){
-    for(int j=0;j<grafo[u].size();j++){
-      int v = grafo[u][j];
-      std::cout << u << " " << v << abs(bandwidth[u] - bandwidth[v]) << std::endl;
-    }
-  }
-  **/
-
-
-  /**
-  for(int i = 0; i < grafo.size(); i++){
-    for(int j = 0; j < grafo[i].size(); j++){
-      std::cout << i << "-" << grafo[i][j] << std::endl;
-    }
+    std::cout << " peso: " << bandwidth[arista.second] << std::endl;
   }
   */
+  
+  //vector max bandwidth
+  std::vector<int> max_bandwidth;
+  std::vector<int> gmax_bandwidth;
+  
+  // ########################SOLUCION NORMAL######################################
+  std::pair<int, int> aux_pivote;
+  std::pair<int, int> aux_pivote2;
 
-  std::pair<int, int> pivote;
-  std::pair<int, int> pivote2;
-  std::cout << "PIVOTE__START" << std::endl;
+  std::pair<int, int> aux2_pivote;
+  std::pair<int, int> aux2_pivote2;
+
+  std::cout << "############################" << std::endl;
+  std::cout << "EVALUACION NORMAL" << std::endl;  
   
   for(int i=0; i<n; i++){
     for(int j=0; j<grafo[i].size(); j++){
       
       //el primer par es pivote
-      pivote = std::make_pair(i, grafo[i][j]);
-      pivote2 = std::make_pair(grafo[i][j],i);
+      aux_pivote = std::make_pair(i, grafo[i][j]);
+      aux_pivote2 = std::make_pair(grafo[i][j],i);
+      int val1,val2;
+      int max_bw;
       //iterar dentro del vector y formar nuevos pares
       for(int k=j+1;k<grafo[i].size();k++){
-	if(map_lista_aristas[pivote] == 0){
-	  std::cout << "(" <<  pivote.first << "," << pivote.second << ":" << map_lista_aristas[pivote2] << ") - (";
+	if(map_lista_aristas[aux_pivote] == 0){
+	  std::cout << "(" <<  aux_pivote.first << "," << aux_pivote.second << ">>" << bandwidth[map_lista_aristas[aux_pivote2]] << ") - (";
+	  val1 = bandwidth[map_lista_aristas[aux_pivote2]];
 	}else{
-	  std::cout << "(" <<  pivote.first << "," << pivote.second << ":" << map_lista_aristas[pivote] << ") - (";
+	  std::cout << "(" <<  aux_pivote.first << "," << aux_pivote.second << ">>" << bandwidth[map_lista_aristas[aux_pivote]] << ") - (";
+	  val1 = bandwidth[map_lista_aristas[aux_pivote]];
 	}
 	
 	//nuevo par
-	std::cout << i << "," << grafo[i][k] << ")" << std::endl;
+	aux2_pivote = std::make_pair(i, grafo[i][k]);
+	aux2_pivote2 = std::make_pair(grafo[i][k],i);
+	if(map_lista_aristas[aux2_pivote] == 0){
+	  std::cout << i << "," << grafo[i][k] << ">>" << bandwidth[map_lista_aristas[aux2_pivote2]]  << ")";
+	  val2 = bandwidth[map_lista_aristas[aux2_pivote2]];
+	}else{
+	  std::cout << i << "," << grafo[i][k] << ">>" << bandwidth[map_lista_aristas[aux2_pivote]] << ")";
+	  val2 = bandwidth[map_lista_aristas[aux2_pivote]];
+	}
+	max_bw = abs(val1-val2);
+	max_bandwidth.push_back(max_bw);
+	std::cout << "=" << max_bw << std::endl;
       }
     }
   }
+  
+  //https://www.geeksforgeeks.org/how-to-find-the-maximum-element-of-a-vector-using-stl-in-c/
+  std::cout << "\nMAX BANDWIDTH:"
+	    << *std::max_element(max_bandwidth.begin(), max_bandwidth.end())
+	    << std::endl;
 
-  std::cout << "PIVOTE__END" << std::endl;
+  // ########################SOLUCION NORMAL######################################
 
-  /**
-  for(auto const& pair: pares){
-    std::cout << "(" << pair.first << "," << pair.second << ")"<< std::endl;
+  std::cout << "\n############################" << std::endl;
+  std::cout << "EVALUACION GREEDY" << std::endl;  
+  
+  // ########################SOLUCION GREEDY######################################
+
+  std::vector<int> gbandwidth(m, 0);
+  
+  for(int i=0;i<grados.size();i++){
+    gbandwidth[grados[i].second] = i+1;
   }
-  **/
   
+  std::cout << "############################" << std::endl;
+  std::cout << "NODO-GRADO" << std::endl;
+
+  //imprimir nodo-grado
+  for (int i = 0; i < grados.size(); i++){
+    std::cout << "NODO: " << grados[i].second
+	      << " GRADO:" << grados[i].first << std::endl;
+  }
   
-  // Imprimir resultado
-  //std::cout << max_bandwidth << std::endl;
-    
+  //Etiquetar respecto al nodo grado
+  std::cout << "LISTA ADJ" << std::endl;
+  for(int i = 0; i < n; i++){
+    std::cout << "NODO " << grados[i].second << " --> [";
+    for(int j = 0; j < grafo[grados[i].second].size(); j++){
+      std::cout << grafo[grados[i].second][j] << " ";
+    }
+    std::cout << "]" << std::endl;
+  }
+
+  //LISTA ARISTAS
+
+  std::pair<int, int> gpesos;
+  //Etiquetar respecto al nodo grado
+  std::cout << "LISTA-ARISTAS" << std::endl;
+  int count = 1;
+  for(int i = 0; i < n; i++){
+    //std::cout << "NODO " << grados[i].second << " --> [";
+    for(int j = 0; j < grafo[grados[i].second].size(); j++){
+      //std::cout << grafo[grados[i].second][j] << " ";
+      if(grados[i].second < grafo[grados[i].second][j]){
+	gpesos = std::make_pair(grados[i].second, grafo[grados[i].second][j]);
+	map_lista_aristas[gpesos] = count;
+	bandwidth[count] = count;
+	std::cout << "(" << grados[i].second << "," << grafo[grados[i].second][j] << ") - "
+		  << map_lista_aristas[gpesos]
+		  << std::endl;
+	//buscar el par y modificar el peso
+	
+	count++;
+      }
+      //buscar el par formado y actualizar su peso
+    }
+  }
+  
+  // ########################SOLUCION GREEDY######################################
+  //EVALUACION
+
+  //hacer una nueva lista de aristas
+  
+  std::pair<int, int> gaux_pivote;
+  std::pair<int, int> gaux_pivote2;
+
+  std::pair<int, int> gaux2_pivote;
+  std::pair<int, int> gaux2_pivote2;
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < grafo[grados[i].second].size(); j++){
+
+      //el primer par es pivote
+      gaux_pivote = std::make_pair(grados[i].second, grafo[grados[i].second][j]);
+      gaux_pivote2 = std::make_pair(grafo[grados[i].second][j],grados[i].second);
+      int gval1,gval2;
+      int gmax_bw;
+      //iterar dentro del vector y formar nuevos pares
+      for(int k=j+1;k<grafo[grados[i].second].size();k++){
+	if(map_lista_aristas[gaux_pivote] == 0){
+	  std::cout << "(" <<  gaux_pivote.first << "," << gaux_pivote.second << ">>" << bandwidth[map_lista_aristas[gaux_pivote2]] << ") - (";
+	  gval1 = bandwidth[map_lista_aristas[gaux_pivote2]];
+	}else{
+	  std::cout << "(" <<  gaux_pivote.first << "," << gaux_pivote.second << ">>" << bandwidth[map_lista_aristas[gaux_pivote]] << ") - (";
+	  gval1 = bandwidth[map_lista_aristas[gaux_pivote]];
+	}
+	
+	//nuevo par
+	gaux2_pivote = std::make_pair(grados[i].second, grafo[grados[i].second][k]);
+	gaux2_pivote2 = std::make_pair(grafo[grados[i].second][k],grados[i].second);
+	if(map_lista_aristas[gaux2_pivote] == 0){
+	  std::cout << grados[i].second << "," << grafo[grados[i].second][k] << ">>" << bandwidth[map_lista_aristas[gaux2_pivote2]]  << ")";
+	  gval2 = bandwidth[map_lista_aristas[gaux2_pivote2]];
+	}else{
+	  std::cout << grados[i].second << "," << grafo[grados[i].second][k] << ">>" << bandwidth[map_lista_aristas[gaux2_pivote]] << ")";
+	  gval2 = bandwidth[map_lista_aristas[gaux2_pivote]];
+	}
+	gmax_bw = abs(gval1-gval2);
+	gmax_bandwidth.push_back(gmax_bw);
+	std::cout << "=" << gmax_bw << std::endl;
+      }
+    }
+  }
+  
+  std::cout << "\nMAX BANDWIDTH:"
+	    << *std::max_element(gmax_bandwidth.begin(), gmax_bandwidth.end())
+	    << std::endl;
   return 0;
+
 }
